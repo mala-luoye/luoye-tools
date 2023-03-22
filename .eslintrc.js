4ce785d311249877539f411efd1759933ff7f9b5
lilian <1220795758@qq.com>
2023-03-22 09:55:39 +0800

    chore: 代码规范工具配置

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  root: true,
+  extends: [
+    "eslint:recommended",
+    "plugin:prettier/recommended",
+    "plugin:@typescript-eslint/recommended"
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
+  plugins: ["@typescript-eslint"],
+  rules: {
+    eqeqeq: 2,
+    "no-alert": 2,
+    "no-undef": 2,
+    "no-use-before-define": 2,
+    "@typescript-eslint/no-explicit-any": 0,
+    "@typescript-eslint/no-non-null-assertion": 0,
+    "@typescript-eslint/no-var-requires": 0
+  }
+}

ab478a95bc278769be0bbfd197274308bc7ae325
lilian <1220795758@qq.com>
2023-03-23 20:43:53 +0800

    docs: 删除不必要的文件

--- packages/document/.docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require('D:\\技术特性测试\\mala-luoye\\luoye-tools\\node_modules\\.pnpm\\infima@0.2.0-alpha.42\\node_modules\\infima\\dist\\css\\default\\default.css'),
-  require('D:\\技术特性测试\\mala-luoye\\luoye-tools\\node_modules\\.pnpm\\@docusaurus+theme-classic@2.3.1_sfoxds7t5ydpegc3knd667wn6m\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages'),
-  require('D:\\技术特性测试\\mala-luoye\\luoye-tools\\node_modules\\.pnpm\\@docusaurus+theme-classic@2.3.1_sfoxds7t5ydpegc3knd667wn6m\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress'),
-  require('D:\\技术特性测试\\mala-luoye\\luoye-tools\\packages\\document\\src\\css\\custom.css'),
-];
